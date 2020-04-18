000480485f9419f3dd1413600c09c57258fe1cdd
Dimitar Dimitrov <dimitrov.dna@gmail.com>
2020-04-18 18:57:20 +0200

    final

--- src/main.java
+++ src/main.java
@@ -1,7 +1,23 @@
+/*
+ *@author mechachki - Dimitar Dimitrov s1018291
+ *@author Carla Schindler s1017233
+ */
 public class main {
     public static void main(String[] args) {
-        Graph random_graph = new Graph(8);
-        Graph manual_graph = new Graph(new int[][]{
+
+        Graph random_graph = new Graph(4,20);
+        System.out.println(random_graph);
+        random_graph.mst();
+
+        Graph random_graph2 = new Graph("Different.txt");
+        System.out.println(random_graph2);
+        random_graph2.mst();
+
+        Graph text_graph = new Graph("graphTwo.txt");
+        System.out.println(text_graph);
+        text_graph.mst();
+
+        Graph hardcoded_graph = new Graph(new int[][]{
                 {0, 4, 0, 0, 0, 0, 0, 8, 0},
                 {4, 0, 8, 0, 0, 0, 0, 11, 0},
                 {0, 8, 0, 7, 0, 4, 0, 0, 2},
@@ -12,14 +28,8 @@ public class main {
                 {8, 11, 0, 0, 0, 0, 1, 0, 7},
                 {0, 0, 0, 0, 0, 0, 6, 7, 0}
                 });
-
-        System.out.println(random_graph);
-
-        int[] trace_random = random_graph.mst(1,7);
-
-        System.out.println(manual_graph);
-
-        int[] trace_manual = manual_graph.mst(0,4);
+        System.out.println(hardcoded_graph);
+        hardcoded_graph.mst();
 
     }
 }
